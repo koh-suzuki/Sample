a451830257dc8919b1b48ff63562462f81aa38c3
kohei suzuki <koh.sss@gmail.com>
2020-04-15 19:14:28 +0900

    home about link_to追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'top', to: 'home#top'
+  root 'home#top'
   get 'about', to: 'home#about'  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
