1c336af9455cb319c2a41e5bee7145dd30ee191b
kohei suzuki <koh.sss@gmail.com>
2020-04-18 00:03:59 +0900

    画像導入&編集 追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_17_093100) do
+ActiveRecord::Schema.define(version: 2020_04_17_135229) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2020_04_17_093100) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
 end
