122b9bc083ba8868cdefb38a2f06a06d156e5a07
kohei suzuki <koh.sss@gmail.com>
2020-04-17 20:19:10 +0900

    user作成 index 追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/index'
   root 'home#top'
   get 'about', to: 'home#about' 
   resources :posts
   post 'posts/:id/update', to: 'posts#update', as: :update_post
   post 'posts/:id/destroy', to: 'posts#destroy', as: :destroy_post
+
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
